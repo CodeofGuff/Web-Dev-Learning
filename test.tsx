15c46430a79718a82fe3e7263de005fb8f54524f
David <161796631+CodeofGuff@users.noreply.github.com>
2024-10-21 21:14:26 -0700

    okay!

---
+++ test.tsx
@@ -0,0 +1,16 @@
+/*rnfes*/
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const test = () => {
+  return (
+    <View>
+      <Text>test</Text>
+    </View>
+  )
+}
+
+export default test
+
+const styles = StyleSheet.create({})

d7d14edd4c26fdb506ba7a052d6c8b00e91d3fb3
David <161796631+CodeofGuff@users.noreply.github.com>
2024-10-26 15:52:51 -0700

    ok

--- JSThings.js
+++ JSThings.js
@@ -1,10 +1,20 @@
 // hello world
 
-
 // Random Number Function
 function number(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-let rnumber = number(1, 999999)
-console.log(rnumber)
+let rnumber = number(1, 999999);
+console.log(rnumber);
 
+function greet(user) {
+  console.log("Hello there" + " " + user);
+  console.log("Welcome back!");
+}
+let david = greet("David");
+console.log(david);
+
+// fizzbuzz
+// for (let i = 1; i <= 100; i++) {
+//   console.log((i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i);
+// }

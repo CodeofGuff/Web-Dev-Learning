8ba03c1806c7a3437a49b2f5eadd0ce9edf05af0
David <161796631+CodeofGuff@users.noreply.github.com>
2025-01-25 14:20:13 -0800

    Rating: 0
    New rating system 0 - 5 on IMportance

--- JSThings.js
+++ JSThings.js
@@ -1,20 +1,10 @@
-// hello world
+const button = document.getElementById("button");
+const secretText = document.getElementById("secretText");
 
-// Random Number Function
-function number(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-let rnumber = number(1, 999999);
-console.log(rnumber);
-
-function greet(user) {
-  console.log("Hello there" + " " + user);
-  console.log("Welcome back!");
-}
-let david = greet("David");
-console.log(david);
-
-// fizzbuzz
-// for (let i = 1; i <= 100; i++) {
-//   console.log((i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i);
-// }
+button.addEventListener("click", () => {
+  if (secretText.style.display === "none") {
+    secretText.style.display = "block";
+  } else {
+    secretText.style.display = "none";
+  }
+});
